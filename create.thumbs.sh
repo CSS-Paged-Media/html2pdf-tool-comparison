aaf174c496883cf77c378ebc35aa9e4c502d7647
Andreas <info@azettl.net>
2020-06-05 03:06:15 +0200

    fix thumbnail script to only generate first page

--- create.thumbs.sh
+++ create.thumbs.sh
@@ -1,4 +1,4 @@
 find . -type f -name '*.pdf' -print0 |
   while IFS= read -r -d '' file
-    do convert -verbose -density 96 -resize 800 "${file}" "${file%.*}.png"
+    do convert -verbose -density 96 -resize 800 "${file}"[0] "${file%.*}.png"
   done
